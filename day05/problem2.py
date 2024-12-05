1eaec7fb7ccb4d6ff79fe5290a066c49c1b78000
JD Huntington <jd@itsthehuntingtons.com>
2024-12-05 13:54:39 -0600

    Tighten things up a bit

--- day05/problem2.py
+++ day05/problem2.py
@@ -4,51 +4,33 @@ from graphlib import TopologicalSorter
 
 def build_global_ordering(pairs):
     graph = {}
+    nodes = {x for pair in pairs for x in pair}
+    graph = {node: set() for node in nodes}
     for before, after in pairs:
-        if after not in graph:
-            graph[after] = set()
-        if before not in graph:
-            graph[before] = set()
         graph[after].add(before)
     return list(TopologicalSorter(graph).static_order())
 
 def middle_number(seq):
     return seq[len(seq) // 2]
     
-def valid_ordering(constraints, test):
-    last_pos = -1
-    for element in test:
-        next_pos = constraints.index(element)
-        if next_pos <= last_pos:
-            return False
-        last_pos = next_pos
-    return True
-
-if __name__ == "__main__":
-    import fileinput
-
+def parse_input():
     rules = []
     updates = []
+    
+    for line in iter(input, ''):
+        rules.append(line.strip())
+    
+    for line in iter(input, ''):
+        updates.append([int(x) for x in line.strip().split(',')])
+        
+    return [tuple(map(int, s.split('|'))) for s in rules], updates
 
-    seen_newline = False
-
-    for line in fileinput.input():
-        if not seen_newline:
-            val = line.strip()
-            if len(val) == 0:
-                seen_newline = True
-            else:
-                rules.append(val)
-        else:
-            updates.append(line.strip())
-
-    rule_pairs = [tuple(map(int, s.split("|"))) for s in rules]
+if __name__ == "__main__":
+    rules, updates = parse_input()
     total = 0
-    for s in updates:
-        vals = [int(x) for x in s.split(",")]
-        applicable_pairs = [(a,b) for a,b in rule_pairs if a in vals and b in vals]
-        correct_ordering = build_global_ordering(applicable_pairs)
-        if not valid_ordering(correct_ordering, vals):
+    for vals in updates:
+        correct_ordering = build_global_ordering([(a,b) for a,b in rules if a in vals and b in vals])
+        if not correct_ordering == vals:
             total += middle_number(correct_ordering)
 
     print(total)
